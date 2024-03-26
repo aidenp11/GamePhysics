c9c1d3680da0a7629d0788ed5824a5ea5f0f8cd9
aidenp11 <129340716+aidenp11@users.noreply.github.com>
2024-03-26 11:10:26 -0600

    first commit

---
+++ Assets/Scripts/PhysicsCollider.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsCollider : MonoBehaviour
+{
+	string status;
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		status = "collision enter : " + collision.gameObject.name;
+	}
+
+	private void OnCollisionStay(Collision collision)
+	{
+
+    }
+
+	private void OnCollisionExit(Collision collision)
+	{
+		status = "";
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		status = "trigger enter : " + other.gameObject.name;
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		status = "";
+	}
+
+	private void OnGUI()
+	{
+		GUI.skin.label.fontSize = 28;
+		Vector2 screen = Camera.main.WorldToScreenPoint(transform.position);
+		GUI.Label(new Rect(screen.x, Screen.height - screen.y, 250, 70), status);
+	}
+}
